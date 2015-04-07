c3139fe76a2c794b1b8916b302fb20739f694303
Kristi Chastain <kchasta2@my.westga.edu>
2015-04-07 16:50:22 -0400

    Condensed code some more in game_controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -54,67 +54,52 @@ class GameController < ApplicationController
       answer_count = @current_game.answers_correct + 1
       @current_game.update_attributes(:turn_count => count, :answers_correct => answer_count)
       @current_game.save!
-
+      @user = User.find(current_user.id)
       if subject == "Art"
-        @user = User.find(current_user.id)
         correct = @user.correct_questions + 1
         art_correct = @user.art_correct_count + 1
         art_total = @user.art_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :art_correct_count => art_correct, :art_total_count => art_total)
-        @user.save!
         @game_id = game_id
-        redirect_to '/game/game?game_id=' + game_id
       end
       if subject == "Entertainment"
-        @user = User.find(current_user.id)
         correct = @user.correct_questions + 1
         ent_correct = @user.entertainment_correct_count + 1
         ent_total = @user.entertainment_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :entertainment_correct_count => ent_correct, :entertainment_total_count => ent_total)
-        @user.save!
         @game_id = game_id
-        redirect_to '/game/game?game_id=' + game_id
       end
       if subject == "History"
-        @user = User.find(current_user.id)
         correct = @user.correct_questions + 1
         history_correct = @user.history_correct_count + 1
         history_total = @user.history_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :history_correct_count => history_correct, :history_total_count => history_total)
-        @user.save!
         @game_id = game_id
-        redirect_to '/game/game?game_id=' + game_id
       end
       if subject == "Geography"
-        @user = User.find(current_user.id)
         correct = @user.correct_questions + 1
         geography_correct = @user.geography_correct_count + 1
         geography_total = @user.geography_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :geography_correct_count => geography_correct, :geography_total_count => geography_total)
-        @user.save!
         @game_id = game_id
-        redirect_to '/game/game?game_id=' + game_id
       end
       if subject == "Science"
-        @user = User.find(current_user.id)
         correct = @user.correct_questions + 1
         science_correct = @user.science_correct_count + 1
         science_total = @user.science_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :science_correct_count => science_correct, :science_total_count => science_total)
-        @user.save!
         @game_id = game_id
-        redirect_to '/game/game?game_id=' + game_id
       end
       if subject == "Sports"
-        @user = User.find(current_user.id)
         correct = @user.correct_questions + 1
         sports_correct = @user.sports_correct_count + 1
         sports_total = @user.sports_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :sports_correct_count => sports_correct, :sports_total_count => sports_total)
-        @user.save!
         @game_id = game_id
-        redirect_to '/game/game?game_id=' + game_id
       end
+      sleep(2)
+      @user.save!
+      redirect_to '/game/game?game_id=' + game_id
     elsif result == 'INCORRECT'
       @game = Game.find(game_id)
       @game.player1_turn=false
