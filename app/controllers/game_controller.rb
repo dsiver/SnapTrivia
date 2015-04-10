2ec7e6672ed2b40fee12b6575ff9c778032826a3
David Siver <dsiver1@my.westga.edu>
2015-04-10 13:16:33 -0400

    Issue #25: Trophies appearing for current_user on game page. They are appearing horizontally. Style issue.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,37 +7,41 @@ class GameController < ApplicationController
 
   # accepts game_id if -1 random user if 0 new game if game_id != 0 || game_id != -1 new game
   def game
+    #remove_player_bonuses(current_user)
     @game_id = params[:game_id]
     users = User.all
     if @game_id.to_i == -1
       player2_id = rand(users.length)
       @player2 = User.find(player2_id)
+      #remove_player_bonuses(@player2)
       @game = Game.new(player1_id: current_user.id, player2_id: @player2.id, player1_turn: true, game_over: false,
                        art_trophy_p1: false, entertainment_trophy_p1: false, history_trophy_p1: false,
                        geography_trophy_p1: false, science_trophy_p1: false, sports_trophy_p1: false,
                        art_trophy_p2: false, entertainment_trophy_p2: false, history_trophy_p2: false,
                        geography_trophy_p2: false, science_trophy_p2: false, sports_trophy_p2: false)
 
-      @player1_badges = get_player_badges(current_user)
-      @player2_badges = get_player_badges(@player2)
+      #@player1_badges = get_player_badges(current_user)
+      #@player2_badges = get_player_badges(@player2)
 
       @game.save!
     else
       if @game_id.to_i == 0
         @player2 = User.find(params[:player2_id])
-
+        #remove_player_bonuses(@player2)
         @game = Game.new(player1_id: current_user.id, player2_id: @player2.id, player1_turn: true, game_over: false,
                          art_trophy_p1: false, entertainment_trophy_p1: false, history_trophy_p1: false,
                          geography_trophy_p1: false, science_trophy_p1: false, sports_trophy_p1: false,
                          art_trophy_p2: false, entertainment_trophy_p2: false, history_trophy_p2: false,
                          geography_trophy_p2: false, science_trophy_p2: false, sports_trophy_p2: false)
 
-        @player1_badges = get_player_badges(current_user)
-        @player2_badges = get_player_badges(@player2)
+        #@player1_badges = get_player_badges(current_user)
+        #@player2_badges = get_player_badges(@player2)
 
         @game.save!
       else
         @game = Game.find(params[:game_id])
+        #@player1_badges = get_player_badges(current_user)
+        #@player2_badges = get_player_badges(@player2)
       end
     end
   end
@@ -59,7 +63,7 @@ class GameController < ApplicationController
     @user.save!
 
     if result == 'CORRECT'
-      give_player_bonus(subject, @user)
+      #give_player_bonus(subject, @user)
 
       @current_game = Game.find(game_id)
       count = @current_game.turn_count + 1
@@ -74,6 +78,7 @@ class GameController < ApplicationController
         art_total = @user.art_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :art_correct_count => art_correct, :art_total_count => art_total)
         @game_id = game_id
+        @current_game.update_attributes(:art_trophy_p1 => true)
       end
       if subject == "Entertainment"
         correct = @user.correct_questions + 1
@@ -81,6 +86,7 @@ class GameController < ApplicationController
         ent_total = @user.entertainment_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :entertainment_correct_count => ent_correct, :entertainment_total_count => ent_total)
         @game_id = game_id
+        @current_game.update_attributes(:entertainment_trophy_p1 => true)
       end
       if subject == "History"
         correct = @user.correct_questions + 1
@@ -88,6 +94,7 @@ class GameController < ApplicationController
         history_total = @user.history_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :history_correct_count => history_correct, :history_total_count => history_total)
         @game_id = game_id
+        @current_game.update_attributes(:history_trophy_p1 => true)
       end
       if subject == "Geography"
         correct = @user.correct_questions + 1
@@ -95,6 +102,7 @@ class GameController < ApplicationController
         geography_total = @user.geography_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :geography_correct_count => geography_correct, :geography_total_count => geography_total)
         @game_id = game_id
+        @current_game.update_attributes(:geography_trophy_p1 => true)
       end
       if subject == "Science"
         correct = @user.correct_questions + 1
@@ -102,6 +110,7 @@ class GameController < ApplicationController
         science_total = @user.science_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :science_correct_count => science_correct, :science_total_count => science_total)
         @game_id = game_id
+        @current_game.update_attributes(:science_trophy_p1 => true)
       end
       if subject == "Sports"
         correct = @user.correct_questions + 1
@@ -109,6 +118,7 @@ class GameController < ApplicationController
         sports_total = @user.sports_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :sports_correct_count => sports_correct, :sports_total_count => sports_total)
         @game_id = game_id
+        @current_game.update_attributes(:sports_trophy_p1 => true)
       end
       sleep(2)
       @user.save!
@@ -174,10 +184,13 @@ class GameController < ApplicationController
 
   # checks params for new game MUST UPDATE!!!
   private
+
+
   def game_params
     params.require(:game).permit(player1_id: current_user.id, player2_id: @player2.id)
   end
 
+=begin
   def get_player_badges(player)
     player.badges.select do |badge|
       badge.custom_fields[:type] == "game_round"
@@ -190,19 +203,31 @@ class GameController < ApplicationController
 
     case subject
       when "Art"
-        user.add_badge(art)
+        user.add_badge(art) unless user.badges.any? { |badge| badge.id == art }
       when "Entertainment"
-        user.add_badge(entertainment)
+        user.add_badge(entertainment) unless user.badges.any? { |badge| badge.id == entertainment }
       when "History"
-        user.add_badge(history)
+        user.add_badge(history) unless user.badges.any? { |badge| badge.id == history }
       when "Geography"
-        user.add_badge(geography)
+        user.add_badge(geography) unless user.badges.any? { |badge| badge.id == geography }
       when "Science"
-        user.add_badge(science)
+        user.add_badge(science) unless user.badges.any? { |badge| badge.id == science }
       when "Sports"
-        user.add_badge(sports)
+        user.add_badge(sports) unless user.badges.any? { |badge| badge.id == sports }
     end
   end
+
+  def remove_player_bonuses(user)
+    # Badge ID's
+    art = 2, entertainment = 3, history = 4, geography = 5, science = 6, sports = 7
+    user.rm_badge(art) if user.badges.any? { |badge| badge.id == art }
+    user.rm_badge(entertainment) if user.badges.any? { |badge| badge.id == entertainment }
+    user.rm_badge(history) if user.badges.any? { |badge| badge.id == history }
+    user.rm_badge(geography) if user.badges.any? { |badge| badge.id == geography }
+    user.rm_badge(science) if user.badges.any? { |badge| badge.id == science }
+    user.rm_badge(sports) if user.badges.any? { |badge| badge.id == sports }
+  end
+=end
 end
 
 
