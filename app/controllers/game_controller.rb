6dc8d1e0ddf44bd5da3699c794557849be33d294
David Siver <dsiver1@my.westga.edu>
2015-04-10 14:42:13 -0400

    Issue #25: refactored to add trophy based upon whether the user is player1 or player2

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,4 @@
 class GameController < ApplicationController
-
   include GameHelper
 
   def index
@@ -7,41 +6,29 @@ class GameController < ApplicationController
 
   # accepts game_id if -1 random user if 0 new game if game_id != 0 || game_id != -1 new game
   def game
-    #remove_player_bonuses(current_user)
+
     @game_id = params[:game_id]
     users = User.all
     if @game_id.to_i == -1
       player2_id = rand(users.length)
       @player2 = User.find(player2_id)
-      #remove_player_bonuses(@player2)
       @game = Game.new(player1_id: current_user.id, player2_id: @player2.id, player1_turn: true, game_over: false,
                        art_trophy_p1: false, entertainment_trophy_p1: false, history_trophy_p1: false,
                        geography_trophy_p1: false, science_trophy_p1: false, sports_trophy_p1: false,
                        art_trophy_p2: false, entertainment_trophy_p2: false, history_trophy_p2: false,
                        geography_trophy_p2: false, science_trophy_p2: false, sports_trophy_p2: false)
-
-      #@player1_badges = get_player_badges(current_user)
-      #@player2_badges = get_player_badges(@player2)
-
       @game.save!
     else
       if @game_id.to_i == 0
         @player2 = User.find(params[:player2_id])
-        #remove_player_bonuses(@player2)
         @game = Game.new(player1_id: current_user.id, player2_id: @player2.id, player1_turn: true, game_over: false,
                          art_trophy_p1: false, entertainment_trophy_p1: false, history_trophy_p1: false,
                          geography_trophy_p1: false, science_trophy_p1: false, sports_trophy_p1: false,
                          art_trophy_p2: false, entertainment_trophy_p2: false, history_trophy_p2: false,
                          geography_trophy_p2: false, science_trophy_p2: false, sports_trophy_p2: false)
-
-        #@player1_badges = get_player_badges(current_user)
-        #@player2_badges = get_player_badges(@player2)
-
         @game.save!
       else
         @game = Game.find(params[:game_id])
-        #@player1_badges = get_player_badges(current_user)
-        #@player2_badges = get_player_badges(@player2)
       end
     end
   end
@@ -63,12 +50,9 @@ class GameController < ApplicationController
     @user.save!
 
     if result == 'CORRECT'
-      #give_player_bonus(subject, @user)
-
       @current_game = Game.find(game_id)
       count = @current_game.turn_count + 1
       answer_count = @current_game.answers_correct + 1
-
       @current_game.update_attributes(:turn_count => count, :answers_correct => answer_count)
       @current_game.save!
       @user = User.find(current_user.id)
@@ -78,7 +62,7 @@ class GameController < ApplicationController
         art_total = @user.art_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :art_correct_count => art_correct, :art_total_count => art_total)
         @game_id = game_id
-        @current_game.update_attributes(:art_trophy_p1 => true)
+        give_trophy(@current_game, "Art", @user)
       end
       if subject == "Entertainment"
         correct = @user.correct_questions + 1
@@ -86,7 +70,7 @@ class GameController < ApplicationController
         ent_total = @user.entertainment_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :entertainment_correct_count => ent_correct, :entertainment_total_count => ent_total)
         @game_id = game_id
-        @current_game.update_attributes(:entertainment_trophy_p1 => true)
+        give_trophy(@current_game, "Entertainment", @user)
       end
       if subject == "History"
         correct = @user.correct_questions + 1
@@ -94,7 +78,7 @@ class GameController < ApplicationController
         history_total = @user.history_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :history_correct_count => history_correct, :history_total_count => history_total)
         @game_id = game_id
-        @current_game.update_attributes(:history_trophy_p1 => true)
+        give_trophy(@current_game, "History", @user)
       end
       if subject == "Geography"
         correct = @user.correct_questions + 1
@@ -102,7 +86,7 @@ class GameController < ApplicationController
         geography_total = @user.geography_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :geography_correct_count => geography_correct, :geography_total_count => geography_total)
         @game_id = game_id
-        @current_game.update_attributes(:geography_trophy_p1 => true)
+        give_trophy(@current_game, "Geography", @user)
       end
       if subject == "Science"
         correct = @user.correct_questions + 1
@@ -110,7 +94,7 @@ class GameController < ApplicationController
         science_total = @user.science_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :science_correct_count => science_correct, :science_total_count => science_total)
         @game_id = game_id
-        @current_game.update_attributes(:science_trophy_p1 => true)
+        give_trophy(@current_game, "Science", @user)
       end
       if subject == "Sports"
         correct = @user.correct_questions + 1
@@ -118,7 +102,7 @@ class GameController < ApplicationController
         sports_total = @user.sports_total_count + 1
         @user.update_attributes!(:correct_questions => correct, :sports_correct_count => sports_correct, :sports_total_count => sports_total)
         @game_id = game_id
-        @current_game.update_attributes(:sports_trophy_p1 => true)
+        give_trophy(@current_game, "Sports", @user)
       end
       sleep(2)
       @user.save!
@@ -188,6 +172,29 @@ class GameController < ApplicationController
 
   def game_params
     params.require(:game).permit(player1_id: current_user.id, player2_id: @player2.id)
+  end
+
+  def give_trophy(game, subject, user)
+    case subject
+      when "Art"
+        game.update_attributes(:art_trophy_p1 => true) when user.id == game.player1_id
+        game.update_attributes(:art_trophy_p2 => true) when user.id == game.player2_id
+      when "Entertainment"
+        game.update_attributes(:entertainment_trophy_p1 => true) when user.id == game.player1_id
+        game.update_attributes(:entertainment_trophy_p2 => true) when user.id == game.player2_id
+      when "History"
+        game.update_attributes(:history_trophy_p1 => true) when user.id == game.player1_id
+        game.update_attributes(:history_trophy_p2 => true) when user.id == game.player2_id
+      when "Geography"
+        game.update_attributes(:geography_trophy_p1 => true) when user.id == game.player1_id
+        game.update_attributes(:geography_trophy_p2 => true) when user.id == game.player2_id
+      when "Science"
+        game.update_attributes(:science_trophy_p1 => true) when user.id == game.player1_id
+        game.update_attributes(:science_trophy_p2 => true) when user.id == game.player2_id
+      when "Sports"
+        game.update_attributes(:sports_trophy_p1 => true) when user.id == game.player1_id
+        game.update_attributes(:sports_trophy_p2 => true) when user.id == game.player2_id
+    end
   end
 
 =begin
