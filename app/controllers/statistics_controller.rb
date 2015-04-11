cf839f6db619faaa2105987f73531ac9e8b1d686
Kristi Chastain <kchasta2@my.westga.edu>
2015-04-10 21:38:12 -0400

    Added site stats page.  Issue #75 - only has overall stats, does not yet have weekly / monthly

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,25 @@
 class StatisticsController < ApplicationController
   def new
   end
+
+  #def user_stats
+   # @user_options = User.all.map{|u| [ u.name, u.id ] }
+  #end
+
+  def site_stats
+    @total_all = 0
+    @total_correct = 0
+    @art_all = 0
+    @art_correct = 0
+    @ent_all = 0
+    @ent_correct = 0
+    @geog_all = 0
+    @geog_correct = 0
+    @hist_all = 0
+    @hist_correct = 0
+    @science_all = 0
+    @science_correct = 0
+    @sports_all = 0
+    @sports_correct = 0
+  end
 end
