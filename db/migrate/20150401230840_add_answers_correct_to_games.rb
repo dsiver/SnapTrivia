f0191d04289e55a08ca299d098107f80e74d4c22
B3Chill <B3Chill@gmail.com>
2015-04-11 21:53:14 -0400

    bonus does not reset answers_correct

--- db/migrate/20150401230840_add_answers_correct_to_games.rb
+++ db/migrate/20150401230840_add_answers_correct_to_games.rb
@@ -1,5 +1,6 @@
 class AddAnswersCorrectToGames < ActiveRecord::Migration
   def change
     add_column :games, :answers_correct, :integer, default: 0
+    add_column :games, :challenge, :string, default: "no"
   end
 end
