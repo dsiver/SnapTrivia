27a0db7a30ef906d9f3984c503b2dec21065735a
David Siver <dsiver1@my.westga.edu>
2015-04-13 12:44:40 -0400

    Issue #7: Moved player trophy logic to the game object so features will be available to Game objects

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ActiveRecord::Base
+
   belongs_to :player1, :class_name => 'User', :foreign_key => 'player1_id'
   belongs_to :player2, :class_name => 'User', :foreign_key => 'player2_id'
 
@@ -8,6 +9,51 @@ class Game < ActiveRecord::Base
   # Verifies that player one and teo are different
   def self.verify_different_players
     errors.add(:player2, "Player 1 and Player 2 must be different users") if :player1_id == :player2_id
+  end
+
+  # Checks to see if the current player can start a challenge
+  # Looks at current player and opponents trophies to see if
+  # challenge can start
+  def can_challenge?(challenger_id, opponent_id)
+    if both_have_only_one_trophy?
+      if same_trophies?
+        @can_challenge = false
+      end
+    else
+      @can_challenge = true
+    end
+    return @can_challenge
+  end
+
+  def player1_trophies
+    @player1_trophies = Array.new
+    @player1_trophies << "Art" if self.art_trophy_p1 == true
+    @player1_trophies << "Entertainment" if self.entertainment_trophy_p1 == true
+    @player1_trophies << "History" if self.history_trophy_p1 == true
+    @player1_trophies << "Geography" if self.geography_trophy_p1 == true
+    @player1_trophies << "Science" if self.science_trophy_p1 == true
+    @player1_trophies << "Sports" if self.sports_trophy_p1 == true
+    return @player1_trophies
+  end
+
+  def player2_trophies
+    @player2_trophies = Array.new
+    @player2_trophies << "Art" if self.art_trophy_p2 == true
+    @player2_trophies << "Entertainment" if self.entertainment_trophy_p2 == true
+    @player2_trophies << "History" if self.history_trophy_p2 == true
+    @player2_trophies << "Geography" if self.geography_trophy_p2 == true
+    @player2_trophies << "Science" if self.science_trophy_p2 == true
+    @player2_trophies << "Sports" if self.sports_trophy_p2 == true
+    return @player2_trophies
+  end
+
+  private
+
+  def both_have_only_one_trophy?
+
+  end
+
+  def same_trophies?
   end
 
   def do_something
