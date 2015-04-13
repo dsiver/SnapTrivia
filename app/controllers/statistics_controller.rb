471909d1ce2bb3b3e5404e436b01fd3d506e324e
Kristi Chastain <kchasta2@my.westga.edu>
2015-04-13 13:15:05 -0400

    Fixed issue when user_id == "" on submit (i.e. - no player selected)

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -5,7 +5,7 @@ class StatisticsController < ApplicationController
   def user_stats
     @user = User.find(current_user.id)
     @users = User.all.where.not(id: [1, current_user.id]).map { |u| [ u.name, u.id ] }
-    if params[:user_id] != nil
+    if params[:user_id] != ""
       @data = User.find(params[:user_id])
     end
 
