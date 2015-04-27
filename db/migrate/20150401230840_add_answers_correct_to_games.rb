50034ec397ecf740cfb66004e6e5b6a608aba9b1
B3Chill <B3Chill@gmail.com>
2015-04-27 13:39:04 -0400

    added boolean flag for bonus to games as games.bonus can be used as current_game.bonus = true/false

--- db/migrate/20150401230840_add_answers_correct_to_games.rb
+++ db/migrate/20150401230840_add_answers_correct_to_games.rb
@@ -2,5 +2,6 @@ class AddAnswersCorrectToGames < ActiveRecord::Migration
   def change
     add_column :games, :answers_correct, :integer, default: 0
     add_column :games, :challenge, :string, default: "no"
+    add_column :games, :bonus, :boolean, default: unquoted_false
   end
 end
