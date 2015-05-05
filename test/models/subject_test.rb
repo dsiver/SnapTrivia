c65c2ab536080afea0ac4db0dd288146ad843e03
David Siver <dsiver1@my.westga.edu>
2015-05-05 16:57:27 -0400

    #7: created a method to return the Subjects in an array for iterating over in ask_question

--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @subjects = [Subject::ART, Subject::ENTERTAINMENT, Subject::GEOGRAPHY, Subject::HISTORY, Subject::SCIENCE, Subject::SPORTS]
+  end
+
+  def teardown
+    @subjects = nil
+  end
+
+  test "Subject::subjects should_not_return_nil_array" do
+    subjects = Subject::subjects
+    assert_not_equal(nil, subjects)
+  end
+
+  test "Subject::subjects should_return_matching_array" do
+    assert_equal(Subject::subjects, @subjects)
+  end
+
+  test "Subject::subjects should_not_return_array_with_subjects_out_of_order" do
+    @subjects.shuffle!
+    assert_not_equal(Subject::subjects, subjects)
+  end
 end
