e685db02c1641f36043c337deb2bc0d46008ac76
David Siver <dsiver1@my.westga.edu>
2015-05-27 15:42:00 -0400

    #91: added configuration settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,17 @@ Snaptrivia::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Mailer Settings
+  config.action_mailer.default_url_options = { host: "https://snaptrivia.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "snaptriviagame@gmail.com",
+      password: "Bill-David-Capstone"
+  }
 end
