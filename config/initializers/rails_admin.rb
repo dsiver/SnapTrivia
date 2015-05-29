7cdb39626fd567de0caa5cda223be6aa739c9c1d
David Siver <dsiver1@my.westga.edu>
2015-05-29 00:00:16 -0400

    removed questions from user/edit in rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -96,7 +96,8 @@ RailsAdmin.config do |config|
                      :art_total_count, :entertainment_correct_count, :entertainment_total_count,
                      :geography_correct_count, :geography_total_count, :history_correct_count,
                      :history_total_count, :science_correct_count, :science_total_count, :sports_correct_count,
-                     :sports_total_count, :score, :next_lvl_score, :level, :total_games, :total_wins, :sash_id
+                     :sports_total_count, :score, :next_lvl_score, :level, :total_games, :total_wins, :sash_id,
+                     :questions
     end
   end
 
