e0e5c5d1ed0213801772fa88b94816bc33ab6e19
David Siver <dsiver1@my.westga.edu>
2015-05-31 13:52:29 -0400

    #88: created rescue from error for application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from StandardError do |e|
+    redirect_to main_app.root_path, :notice => 'An unexpected error has occurred.'
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
   end
